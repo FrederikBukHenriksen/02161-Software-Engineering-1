dec83ef543b397a5fa07c58ed5860008af39136c
Gustavclausen1804 <73382996+Gustavclausen1804@users.noreply.github.com>
2022-03-15 19:51:02 +0100

    Changed location + added test file

---
+++ Tests/createProjectSteps.java
@@ -0,0 +1,36 @@
+import static org.junit.Assert.assertEquals;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ExampleSteps {
+
+	Project project = new Project();
+
+	@Given("the first input is {String}")
+	public void theFirstInputIs(String project_name) {
+		project.setName(project_name);
+	}
+
+	@And("the second input is {String}")
+	public void theSecondInputIs(String project_start_date) {
+		project.setStartDate(project_start_date);
+	}
+
+	@And("the third input is {String}")
+	public void theThirdInputIs(String project_end_date) {
+		project.setEndDate(project_end_date);
+	}
+
+	@When("the add button is pressed")
+	public void theAddButtonIsPressed() {
+		project.add();
+	}
+
+	// Should test if the project is in the Project ArrayList. However, i'm not sure yet if this is saved in an Arraylist....
+	@Then("{String} is shown on the display.")
+	public void theIsShown(Integer number) {
+		for each Project in <ProjectList>
+	    assertEquals((int)number,calc.display());
+	}
