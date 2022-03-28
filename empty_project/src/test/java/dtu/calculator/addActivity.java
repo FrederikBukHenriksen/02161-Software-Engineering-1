d4ebf42a82313e9c58357081b4e84d261e6be531
Niklas <niklasnjohansen@gmail.com>
2022-03-28 17:49:10 +0200

    Added addActivity.java but it caused problems
    Its mere existance among the other java test-files breaks the tests:
    I ran the tests without the file existing - thhings worked, 1/1 scenarios, 4/4 steps.
    I created the new java-file and pasted the code in (that's it) - tests fail.
    Did I curse the test code??

---
+++ empty_project/src/test/java/dtu/calculator/addActivity.java
@@ -0,0 +1,43 @@
+package dtu.calculator;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class addActivity {
+    
+    Project project;
+
+    @Given("that the project leader is logged in")
+    public void that_the_project_leader_is_logget_in() {
+
+    }
+
+    @Given("there is a project titled {projectTitle} with id {projectID}")
+    public void there_is_a_project_titled_with_id(String projectTitle, int projectID) {
+
+    }
+
+    @Given("there is an activity titled {activityTitle}")
+    public void there_is_an_activity_titled(String activityTitle) {
+
+    }
+
+    @Given("the activity is not on the list of activities")
+    public void the_activity_is_not_on_the_list_of_activities() {
+
+    }
+
+    @When("the activity is added to the list of activities")
+    public void the_activity_is_added_to_the_list_of_activities() {
+
+    }
+
+    @Then("the activity is added to list of activities for the project")
+    public void the_activity_is_added_to_list_of_activities_for_the_project() {
+
+    }
+
+}
