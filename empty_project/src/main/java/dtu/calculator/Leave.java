3a08370d9155558dcc330c03798c9eac9ea3167c
FrederikBukHenriksen <frederikbuk@gmail.com>
2022-03-28 14:01:52 +0200

    Komplet UML i filer.

---
+++ empty_project/src/main/java/dtu/calculator/Leave.java
@@ -0,0 +1,12 @@
+package dtu.calculator;
+
+import java.util.GregorianCalendar;
+
+public class Leave extends Registration {
+
+    public Leave(GregorianCalendar startTime, GregorianCalendar endTime) {
+        super(startTime, endTime);
+        // TODO Auto-generated constructor stub
+    }
+
+}
