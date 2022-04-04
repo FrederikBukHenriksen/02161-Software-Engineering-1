d6b98f8bbea65788496014d2af47e95080b4435a
Niklas <niklasnjohansen@gmail.com>
2022-04-04 15:31:34 +0200

    First attempt at implementing addActivity
    Wrote some code for adding activities, but VS Code doens't seem to return anything in the debug console, and just states that addEmployee works, while my own feature fails all scenarios. The debug console just says 0 Scenarios and 0 Steps...

--- empty_project/src/test/java/dtu/calculator/addActivity.java
+++ empty_project/src/test/java/dtu/calculator/addActivity.java
@@ -1,43 +1,62 @@
 package dtu.calculator;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class addActivity {
-    
+
     Project project;
-
+    Activity activity;
+        
     @Given("that the project leader is logged in")
-    public void that_the_project_leader_is_logget_in() {
-
+    public void that_the_project_leader_is_logged_in() {
+        assertTrue(true); // placeholder until I figure out where we get/create the projectleader (in Employee- or Project-class)
     }
 
     @Given("there is a project titled {projectTitle} with id {projectID}")
-    public void there_is_a_project_titled_with_id(String projectTitle, int projectID) {
-
+    public void there_is_a_project_titled_with_id(String projectTitle, String projectID) {
+        project = new Project(projectTitle);
+        project.id = Integer.parseInt(projectID);
     }
 
     @Given("there is an activity titled {activityTitle}")
     public void there_is_an_activity_titled(String activityTitle) {
-
+        activity = new Activity(activityTitle);
     }
 
     @Given("the activity is not on the list of activities")
     public void the_activity_is_not_on_the_list_of_activities() {
-
+        assertTrue(project.activities.stream().noneMatch(act -> act.title.equals(activity.title)));
     }
 
     @When("the activity is added to the list of activities")
     public void the_activity_is_added_to_the_list_of_activities() {
-
+        project.activities.add(activity);
     }
 
-    @Then("the activity is added to list of activities for the project")
-    public void the_activity_is_added_to_list_of_activities_for_the_project() {
-
+    @Then("the activity exists in the list of activites for the project")
+    public void the_activity_exists_in_the_list_of_activities_for_the_project() {
+        assertTrue(project.activities.stream().anyMatch(act -> act.title.equals(activity.title)));
     }
+
+
+    // @Given("that the project leader is not logged in")
+    // public void that_the_project_leader_is_not_logged_in() {
+    //     assertFalse(false);
+    // }
+
+    // @Then("the error message {string} is given")
+    // public void the_error_message_is_given(String string) {
+        
+    // }
+
+    // @Given("the activity is already on the list of activities")
+    // public void the_activity_is_already_on_the_list_of_activities() {
+        
+    // }
 
 }
