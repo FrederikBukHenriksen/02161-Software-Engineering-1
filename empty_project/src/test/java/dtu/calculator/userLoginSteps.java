ec7117880f9996b1b376296361e897c236ceee11
FrederikBukHenriksen <62750296+FrederikBukHenriksen@users.noreply.github.com>
2022-04-12 14:54:10 +0200

    Static og udtræk af mange steps

--- empty_project/src/test/java/dtu/calculator/userLoginSteps.java
+++ empty_project/src/test/java/dtu/calculator/userLoginSteps.java
@@ -1,7 +1,6 @@
 package dtu.calculator;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.notNull;
 
 import java.util.ArrayList;
 import io.cucumber.java.en.Given;
