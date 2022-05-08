9aada05f35c23f8e5a0597ba4bd2fad7fa742205
FrederikBukHenriksen <62750296+FrederikBukHenriksen@users.noreply.github.com>
2022-05-08 17:58:31 +0200

    sdfsd

--- empty_project/src/main/java/dtu/calculator/CustomCalendar.java
+++ empty_project/src/main/java/dtu/calculator/CustomCalendar.java
@@ -7,24 +7,24 @@ public class CustomCalendar extends GregorianCalendar {
     GregorianCalendar calendar = new GregorianCalendar();
 
     protected CustomCalendar(int year, int week) throws Exception {
+        setTimeZero();
         setYear(year);
         setWeek(week);
-        setTimeZero();
     }
 
     protected CustomCalendar(int year, int month, int date) {
+        setTimeZero();
         setYear(year);
         setMonth(month);
         setDate(date);
         setTimeZero();
-
     }
 
     protected CustomCalendar(int year, int month, int date, int hour, int minute) {
+        setTimeZero();
         setYear(year);
         setMonth(month);
         setDate(date);
-        setTimeZero();
         setHour(hour);
         setMinute(minute);
     }
