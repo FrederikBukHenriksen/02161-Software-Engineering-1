eb9e21277a1a382fda5002c436b3e40cc0ea026d
FrederikBukHenriksen <62750296+FrederikBukHenriksen@users.noreply.github.com>
2022-05-08 16:28:15 +0200

    Register works

--- empty_project/src/main/java/dtu/calculator/CustomCalendar.java
+++ empty_project/src/main/java/dtu/calculator/CustomCalendar.java
@@ -6,32 +6,31 @@ public class CustomCalendar extends GregorianCalendar {
 
     GregorianCalendar calendar = new GregorianCalendar();
 
-    public CustomCalendar(int year, int week) throws Exception {
+    protected CustomCalendar(int year, int week) throws Exception {
         setYear(year);
         setWeek(week);
     }
 
-    public CustomCalendar(int year, int month, int day) {
-        calendar = new GregorianCalendar(year, month - 1, day);
+    protected CustomCalendar(int year, int month, int date) {
+        setYear(year);
+        setMonth(month);
+        setDate(date);
     }
 
-    public CustomCalendar(int year, int month, int day, int hour, int minute) {
-        calendar = new GregorianCalendar(year, month - 1, day, hour, minute);
+    protected CustomCalendar(int year, int month, int date, int hour, int minute) {
+        setYear(year);
+        setMonth(month);
+        setDate(date);
+        setHour(hour);
+        setMinute(minute);
     }
 
-    public int getYear() {
-        return calendar.get(GregorianCalendar.YEAR);
-    }
-
-    public int getWeek() {
-        return calendar.get(GregorianCalendar.WEEK_OF_YEAR);
-    }
-
-    public void setYear(int year) {
+    // Set functions
+    protected void setYear(int year) {
         calendar.set(GregorianCalendar.YEAR, year);
     }
 
-    public void setWeek(int week) throws Exception {
+    protected void setWeek(int week) throws Exception {
         int maxWeekNumber = calendar.getActualMaximum(GregorianCalendar.WEEK_OF_YEAR);
         if (week < 1 || week > maxWeekNumber) {
             throw new Exception("Week number not valid");
@@ -39,16 +38,33 @@ public class CustomCalendar extends GregorianCalendar {
         calendar.set(GregorianCalendar.WEEK_OF_YEAR, week);
     }
 
+    protected void setMonth(int month) {
+        calendar.set(GregorianCalendar.MONTH, month + 1);
+    }
+
+    protected void setDate(int date) {
+        calendar.set(GregorianCalendar.DAY_OF_MONTH, date);
+    }
+
+    protected void setHour(int hour) {
+        calendar.set(GregorianCalendar.HOUR_OF_DAY, hour);
+    }
+
+    protected void setMinute(int minute) {
+        calendar.set(GregorianCalendar.MINUTE, minute);
+    }
+
+    // Get functions
     protected GregorianCalendar getDate() {
         return calendar;
     }
 
-    public void setMonth(int month) throws Exception {
-        int maxWeekNumber = calendar.getActualMaximum(GregorianCalendar.MONTH) + 1;
-        if (month > maxWeekNumber) {
-            throw new Exception("month not valid");
-        }
+    protected int getYear() {
+        return calendar.get(GregorianCalendar.YEAR);
+    }
 
+    protected int getWeek() {
+        return calendar.get(GregorianCalendar.WEEK_OF_YEAR);
     }
 
 }
